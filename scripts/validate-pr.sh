cceb8803d918918fb10f552a8cc5ce13b4582ee5
Thomas Darimont <thomas.darimont@codecentric.de>
2021-09-23 13:28:26 +0200

    Add github action for validate PRs and commit messages

---
+++ scripts/validate-pr.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+: "${PR_TITLE:?Environment variable must be set}"
+
+main() {
+    
+    if [[ ! "$PR_TITLE" =~ "^KEYCLOAK-[0-9]{6} .{1,120}$" ]]; then
+        echo "PR title must start with KEYCLOAK-issue followed by a space and the message! Example: 'KEYCLOAK-1234 Adds new feature'." >&2
+        exit 1
+    fi
+}
+
+main
